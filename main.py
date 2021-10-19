126315c6cef5a72929a7920e310a83daee07a6fe
vladikshkwok <vladikshkwok@gmail.com>
2021-10-19 20:49:24 +0300

    Added first variation of registration (I think I'll redo this)

--- main.py
+++ main.py
@@ -1,41 +1,12 @@
+import datetime
+
+import telegram.utils.request
 from telegram import Update, ReplyKeyboardRemove, Bot, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CallbackContext, Filters, MessageHandler
 from telegram.utils.request import Request
+
+from mysql_func import new_user, find_user
 from settings import TG_TOKEN
-
-button_help = 'Помощь'
-
-
-def button_help_handler(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        text='Ты думал я кнопки обрабатываю? Наивный',
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-def message_handler(update: Update, context: CallbackContext):
-    text = update.message.text
-    if text == button_help:
-        return button_help_handler(update=update, context=context)
-    reply_markup = ReplyKeyboardMarkup(
-        keyboard=[
-            [
-                KeyboardButton(text=button_help),
-            ],
-            [
-                KeyboardButton(text="Хеллоу"),
-            ],
-            [
-                KeyboardButton(text=button_help),
-            ],
-        ],
-        resize_keyboard=True,
-    )
-
-    update.message.reply_text(
-        text='Hello, press button below',
-        reply_markup=reply_markup,
-    )
 
 
 def log_errors(f):
@@ -47,6 +18,70 @@ def log_errors(f):
             raise e
 
     return inner
+
+
+button_help = 'Помощь'
+button_register = 'Зарегистрироваться'
+
+
+def button_help_handler(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text='Добрый день, вас приветствует бот. Для управления ботом требуется использовать клавиатуру',
+    )
+
+
+def button_register_handler(update: Update, context: CallbackContext):
+    username = update.message.from_user.username
+    first_name = update.message.from_user.first_name
+    last_name = update.message.from_user.last_name
+    phone = update.message.contact.phone_number
+    if first_name is None:
+        first_name = "Pass"
+    if last_name is None:
+        last_name = "Pass"
+    user_id = update.message.from_user.id
+    new_user(user_full_name=first_name + ' ' + last_name, tg_username=username, tg_user_id=user_id)
+    update.message.reply_text(
+        text=f'Добрый день, вы занесены в базу данных, с именем пользователя: {username} и телефоном {phone}',
+    )
+
+
+def message_handler(update: Update, context: CallbackContext):
+    text = update.message.text
+    contact = update.message.contact
+    tg_user_id = update.message.from_user.id
+    print(f'User with tg_id: {tg_user_id} requested: {text}')
+    if text == button_help:
+        return button_help_handler(update=update, context=context)
+    if text == 'test':
+        update.message.reply_text(
+            text='Hello, this was test',
+        )
+        return
+    reply_markup = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text=button_register, request_contact=True, ),
+            ],
+            [
+                KeyboardButton(text=button_help),
+            ],
+            [
+                KeyboardButton(text='test'),
+            ],
+        ],
+        resize_keyboard=True,
+    )
+    if contact is not None:
+        reply_markup.keyboard.remove([KeyboardButton(text=button_register, request_contact=True, )])
+        update.message.reply_text(text='Регистрация', reply_markup=reply_markup, )
+        return button_register_handler(update=update, context=context)
+    if find_user(tg_user_id=tg_user_id):
+        reply_markup.keyboard.remove([KeyboardButton(text=button_register, request_contact=True, )])
+    update.message.reply_text(
+        text='Hello, press button below',
+        reply_markup=reply_markup,
+    )
 
 
 @log_errors
@@ -64,10 +99,8 @@ def main():
         bot=bot,
         use_context=True,
     )
-
     print(f'Info about bot: {updater.bot.get_me()}')
     updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
-
     updater.start_polling()
     updater.idle()
 
---
+++ mysql_func.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+connection = mysql.connector.connect(host='localhost', user='gtionline', password='tetraroot', database='tetragigs')
+
+
+def mysql_exec(db_query, db_connection=connection):
+    try:
+        with db_connection.cursor() as cursor:
+            cursor.execute(db_query)
+            return cursor.fetchall()
+    except Exception as e:
+        print(e)
+
+
+def new_user(user_full_name, tg_username, tg_user_id):
+    db_query = f"INSERT INTO user (full_name,tg_user_name,register_date,tg_user_id) values ('{user_full_name}', '{tg_username}', NOW(), '{tg_user_id}');"
+    print(db_query)
+    res = mysql_exec(db_query=db_query)
+    print(res)
+
+
+def find_user(tg_user_id):
+    db_query = f"select id from user where tg_user_id={tg_user_id};"
+    res = mysql_exec(db_query=db_query)
+    if res.__len__() == 1:
+        return True
+    if res.__len__() > 1:
+        print("Записей пользователя больше чем одна, что то не так")
+    return False
